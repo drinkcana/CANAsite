53e03e7e3ebea232b4e7e6814216ad7600f25b2f
aparelho <liga@aparelho.tv>
2020-05-08 18:50:38 -0300

    added tips & tircks snippet

---
+++ site/snippets/tipstricks.php
@@ -0,0 +1,40 @@
+<div class="tips-wrapper shadowed">
+
+	<div class="folder-caller shadowed">Tips & Tricks</div>
+	<p class="tips-intro">Making cocktails at home can be intimidating if you've never tried, or are worried you don't have the right equipment. But fear not! We've got a couple tips and tricks to help you get started as an amateur mixologist at home.</p>
+
+	<div class="tips-holder">
+
+		<div class="tip float-left">
+			<h3>Shaking</h3> 
+			Use any two glasses that 
+			fit well together.
+			Careful not to break ’em.
+		</div>
+		<div class="tip float-left">
+				<h3>Stirring</h3> 
+			Use any of your  favorite spoons.
+		</div>
+		<div class="tip float-left">
+				<h3>Muddling</h3> 
+			You can use a wooden spon, a blender, or a very agitated dog.
+		</div>
+		<div class="tip float-left">
+			<h3>Measuring</h3> 
+			It’s simple, 1 oz equals two tablespoons.
+		</div>
+		<div class="tip float-left">
+			<h3>Straining</h3> 
+			Don’t tell anyone, but any kind of food strainer will work. Just make sure they are properly washed, you big mess.
+		</div>
+		<div class="tip float-left">
+			<h3>Symple Syrup</h3> 
+			<span class="ingredients">1 CUP OF WATER  X  1 CUP OF SUGAR </span>
+
+In a small saucepan, bring sugar and water to a boil; simmer until the sugar is dissolved, ~3 minutes. Remove from the heat and let cool completely.
+		</div>
+
+
+	</div>
+
+</div>
