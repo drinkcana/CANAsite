e10f5a7297d64b6a3f5384be2cec377e8670f749
Nick Walker <nick@drinkcana.com>
2020-05-21 18:25:25 -0300

    Adding Google Tag Manager Code

--- site/snippets/header-social-snippets.php
+++ site/snippets/header-social-snippets.php
@@ -32,6 +32,14 @@
   gtag('config', 'UA-127157768-3');
 </script>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WVLS23V');</script>
+<!-- End Google Tag Manager -->
+
 <!-- Twitter universal website tag code -->
 <script>
 !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
